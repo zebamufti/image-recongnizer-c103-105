beff4bbedcfe6327ffc0d3190712754a221de585
zebamufti <95521076+zebamufti@users.noreply.github.com>
2021-12-04 12:56:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_uri+"'/>";
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+
+  function check(){
+      img=document.getElementById("captured_image")
+ classifier.classify(img,gotresult)
+    }
+    function gotresult(error,result){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(result)
+        document.getElementById("result_object_name").innerHTML=result[0].label
+        document.getElementById("result_object_accuracy").innerHTML=(result[0].confidence*100).toFixed(2);
+    }
+    }
+  
